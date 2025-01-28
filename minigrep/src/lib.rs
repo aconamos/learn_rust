752c7b781225dd1d03d9a2f54a872af4d5f9d5b7
nephin <nephi.norton@gmail.com>
2025-01-27 20:53:58 -0700

    ch10..=ch12.4 - functional minigrep + my own stdin twist

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,92 @@
+use std::{
+    error::Error,
+    fs,
+    io::{Read, Stdin},
+};
+
+enum InputStream {
+    FilePath(String),
+    StdInStream(Stdin), // TODO: Refactor to use anything implementing the Read trait
+}
+
+pub struct Config {
+    query: String,
+    input: InputStream,
+}
+
+impl Config {
+    pub fn from_args_slice(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            Err("Too few arguments!")
+        } else if args.len() < 3 {
+            Ok(Config::from(
+                &args[1],
+                InputStream::StdInStream(std::io::stdin()),
+            ))
+        } else {
+            Ok(Config::from(
+                &args[1],
+                InputStream::FilePath(args[2].clone()),
+            ))
+        }
+    }
+
+    fn from(query: &String, input: InputStream) -> Config {
+        Config {
+            query: query.clone(),
+            input,
+        }
+    }
+
+    pub fn from_str(query: &String, input: &String) -> Config {
+        Config {
+            query: query.clone(),
+            input: InputStream::FilePath(input.clone()),
+        }
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = match config.input {
+        InputStream::FilePath(str) => fs::read_to_string(str)?,
+        InputStream::StdInStream(mut str) => {
+            let mut contents = String::new();
+            str.read_to_string(&mut contents)?;
+            contents
+        }
+    };
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,39 +1,17 @@
-use std::{env, fs};
+use std::{env, process};
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn from_args_slice(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            Err("Too few arguments!")
-        } else {
-            Ok(Config::from(&args[1], &args[2]))
-        }
-    }
-
-    fn from(query: &String, file_path: &String) -> Config {
-        Config {
-            query: query.clone(),
-            file_path: file_path.clone(),
-        }
-    }
-}
+use minigrep::{self, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::from_args_slice(&args).unwrap();
+    let config = Config::from_args_slice(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!(
-        "Searching for: {}
-    In file path: {}",
-        config.query, config.file_path
-    );
-
-    let contents = fs::read_to_string(config.file_path).expect("File read err");
-
-    println!("{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
