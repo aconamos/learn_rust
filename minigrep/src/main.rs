6a2b972e39aa36861428ace3d50052f82242b224
nephi@traveling-circus-wsl <nephi.norton@gmail.com>
2025-01-27 18:23:07 -0700

    minigrep progress

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,39 @@
+use std::{env, fs};
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn from_args_slice(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            Err("Too few arguments!")
+        } else {
+            Ok(Config::from(&args[1], &args[2]))
+        }
+    }
+
+    fn from(query: &String, file_path: &String) -> Config {
+        Config {
+            query: query.clone(),
+            file_path: file_path.clone(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::from_args_slice(&args).unwrap();
+
+    println!(
+        "Searching for: {}
+    In file path: {}",
+        config.query, config.file_path
+    );
+
+    let contents = fs::read_to_string(config.file_path).expect("File read err");
+
+    println!("{}", contents);
 }
